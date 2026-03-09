e7d83daf030221428c347041484c4755023097a9
mahmadsadikRepositry <sadikkoliya@gmail.com>
2026-03-09 12:45:01 -0400

    Datatypes in java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,27 @@ class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        //Premetive Data Types in Java
+        
+        //Integer
+        int age = 20;
+
+        //Long
+        long mobileNumber = 9876543210L;
+
+        //Decimal
+        float price = 10.5f;
+        double salary = 25000.75;
+
+        //Char
+        char grade = 'A';
+
+        //boolean
+        boolean isPassed = true;
+
+        
     }
 }
+
+
+    
